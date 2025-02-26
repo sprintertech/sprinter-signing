6e22f4e7edb01706b94d513d8d12cff8eb3728d7
mpetrun5 <matija.petrunic@gmail.com>
2025-02-26 13:31:54 +0100

    Ignore valid json encode errors

--- api/handlers/util.go
+++ api/handlers/util.go
@@ -9,5 +9,5 @@ func JSONError(w http.ResponseWriter, err interface{}, code int) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.Header().Set("X-Content-Type-Options", "nosniff")
 	w.WriteHeader(code)
-	json.NewEncoder(w).Encode(err)
+	_ = json.NewEncoder(w).Encode(err)
 }
