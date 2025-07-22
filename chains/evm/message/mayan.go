a43bcc0a6eb43e64ee3c3c4b26719d5a0d0fa340
mj52951 <matija.jelavic@chainsafe.io>
2025-07-22 12:13:37 +0200

    fetch by url tx hash

--- chains/evm/message/mayan.go
+++ chains/evm/message/mayan.go
@@ -111,7 +111,7 @@ func (h *MayanMessageHandler) HandleMessage(m *message.Message) (*proposal.Propo
 		data.ErrChn <- err
 		return nil, err
 	}
-	swap, err := h.swapFetcher.GetSwap(data.OrderHash)
+	swap, err := h.swapFetcher.GetSwap(data.DepositTxHash)
 	if err != nil {
 		data.ErrChn <- err
 		return nil, err
