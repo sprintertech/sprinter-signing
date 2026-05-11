11533ad29abf6d5cceb485d884412e40f4675f75
Matija Petrunić <matija.petrunic@gmail.com>
2026-05-11 12:04:43 +0200

    fix: reduce process initiate time (#186)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Description
    <!--- Describe your changes in detail -->
    
    ## Related Issue Or Context
    <!--- If suggesting a new feature or change, please discuss it in an
    issue first -->
    <!--- If fixing a bug, there should be an issue describing it with steps
    to reproduce -->
    <!--- Otherwise, describe context and motivation for change herre -->
    
    Closes: #184 
    
    ## How Has This Been Tested? Testing details.
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, and the tests you ran
    to -->
    <!--- see how your change affects other areas of the code, etc. -->
    
    ## Types of changes
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)
    - [ ] Documentation
    
    ## Checklist:
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    - [ ] I have commented my code, particularly in hard-to-understand
    areas.
    - [ ] I have ensured that all acceptance criteria (or expected behavior)
    from issue are met
    - [ ] I have updated the documentation locally and in docs.
    - [ ] I have added tests to cover my changes.
    - [ ] I have ensured that all the checks are passing and green, I've
    signed the CLA bot
    
    ---------
    
    Co-authored-by: Marin Petrunić <mpetrunic@users.noreply.github.com>

--- tss/coordinator.go
+++ tss/coordinator.go
@@ -7,6 +7,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"slices"
 	"sync"
 	"time"
 
@@ -18,11 +19,10 @@ import (
 	"github.com/sprintertech/sprinter-signing/comm/elector"
 	"github.com/sprintertech/sprinter-signing/tss/ecdsa/common"
 	"github.com/sprintertech/sprinter-signing/tss/message"
-	"golang.org/x/exp/slices"
 )
 
 var (
-	initiatePeriod = 1 * time.Second
+	initiatePeriod = 100 * time.Millisecond
 )
 
 type TssProcess interface {
@@ -185,10 +185,10 @@ func (c *Coordinator) start(
 }
 
 // broadcastInitiateMsg sends TssInitiateMsg to all peers
-func (c *Coordinator) broadcastInitiateMsg(sessionID string) {
+func (c *Coordinator) broadcastInitiateMsg(sessionID string, peers peer.IDSlice) {
 	log.Debug().Str("SessionID", sessionID).Msgf("broadcasted initiate message")
 	_ = c.communication.Broadcast(
-		c.host.Peerstore().Peers(), []byte{}, comm.TssInitiateMsg, sessionID,
+		peers, []byte{}, comm.TssInitiateMsg, sessionID,
 	)
 }
 
@@ -212,7 +212,7 @@ func (c *Coordinator) initiate(
 	ticker := time.NewTicker(c.InitiatePeriod)
 	defer ticker.Stop()
 	initiateStart := time.Now()
-	c.broadcastInitiateMsg(tssProcess.SessionID())
+	go c.broadcastInitiateMsg(tssProcess.SessionID(), c.host.Peerstore().Peers())
 	for {
 		select {
 		case err := <-errChn:
@@ -237,14 +237,25 @@ func (c *Coordinator) initiate(
 					return err
 				}
 
-				_ = c.communication.Broadcast(c.host.Peerstore().Peers(), startMsgBytes, comm.TssStartMsg, tssProcess.SessionID())
+				go func() {
+					_ = c.communication.Broadcast(c.host.Peerstore().Peers(), startMsgBytes, comm.TssStartMsg, tssProcess.SessionID())
+				}()
 				c.metrics.RecordInitiateDuration(time.Since(initiateStart))
 				ticker.Stop()
 				go c.startProcess(ctx, tssProcess, true, startParams, resultChn, errChn)
 			}
 		case <-ticker.C:
 			{
-				c.broadcastInitiateMsg(tssProcess.SessionID())
+				unreadyPeers := make([]peer.ID, 0)
+				for _, p := range readyPeers {
+					if slices.Contains[peer.IDSlice](readyPeers, p) {
+						continue
+					}
+
+					unreadyPeers = append(unreadyPeers, p)
+				}
+
+				go c.broadcastInitiateMsg(tssProcess.SessionID(), unreadyPeers)
 			}
 		case <-ctx.Done():
 			{
@@ -281,9 +292,11 @@ func (c *Coordinator) waitForStart(
 				}
 
 				log.Debug().Str("SessionID", tssProcess.SessionID()).Msgf("sent ready message to %s", wMsg.From)
-				_ = c.communication.Broadcast(
-					peer.IDSlice{wMsg.From}, []byte{}, comm.TssReadyMsg, tssProcess.SessionID(),
-				)
+				go func() {
+					_ = c.communication.Broadcast(
+						peer.IDSlice{wMsg.From}, []byte{}, comm.TssReadyMsg, tssProcess.SessionID(),
+					)
+				}()
 			}
 		case err := <-errChn:
 			return err
